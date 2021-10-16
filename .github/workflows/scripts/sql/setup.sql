4ec96fd7580a68733f07ffb52d1d3b8c33160b59
fortissimo1997 <fortissimo1997@gmail.com>
2021-10-16 22:24:48 +0900

    fix sql

--- .github/workflows/scripts/sql/setup.sql
+++ .github/workflows/scripts/sql/setup.sql
@@ -1,4 +1,4 @@
 use sample;
 
-create table dummy_table (id int(11) not null auto_increment, title varchra(255) not null, primary key (id));
+create table dummy_table (id int(11) not null auto_increment, title varchar(255) not null, primary key (id));
 insert into dummy_table (title) values ('foo'), ('bar'), ('baz');
